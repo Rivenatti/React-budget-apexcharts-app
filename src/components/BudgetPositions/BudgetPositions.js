0956df3968352880687cdbe9b3b59ce67254eb80
Marcin <Rivenatti@gmail.com>
2018-10-25 19:57:10 +0200

    functionality done

--- src/components/BudgetPositions/BudgetPositions.js
+++ src/components/BudgetPositions/BudgetPositions.js
@@ -1,77 +1,106 @@
-import React from "react";
+import React, { Component } from "react";
 import "./BudgetPositions.css";
 import trash_icon from "../Icons/trash-icon.png";
 import edit_icon from "../Icons/edit-icon.png";
 
-const budgetPositions = props => {
+class BudgetPositions extends Component {
+  state = {
+    filterDesc: null,
+    filteredPositions: []
+  };
+
   // HANDLE DELETE
-  const handleDelete = description => {
-    props.delete(description);
+  handleDelete = description => {
+    this.props.delete(description);
   };
 
   // FILTER IF STYLE OF THE POSITION IS POSITIVE OR NEGATIVE
-  const filteredPositionStyle = value => {
+  filteredPositionStyle = value => {
     return value >= 0
       ? "budget__position budget__position--positive"
       : "budget__position budget__position--negative";
   };
 
-  return (
-    <div className="budget__history">
-      <h1>History</h1>
-      <div className="budget__history--filter">
-        <span>Find: </span>
-        <input
-          type="text"
-          name="filter_description"
-          placeholder="description..."
-        />
-      </div>
-      <div className="budget__table">
-        <div className="budget__header">
-          <div className="budget__header__key">ID</div>
-          <div className="budget__header__desc">Description</div>
+  filterDesc = event => {
+    this.setState({
+      ...this.state,
+      filterDesc: event.target.value
+    });
+  };
 
-          <div className="budget__header__cost">Cost</div>
+  filterPositions = () => {
+    // CREATE COPY OF THE POSITIONS FROM PROPS
+    let positions = this.props.positions;
 
-          <div className="budget__header__buttons">Action</div>
+    // IF FILTERED DESCRIPTION GIVEN THEN FILTER THE POSITIONS
+    if (this.state.filterDesc !== null) {
+      return positions.filter(position =>
+        position.description.includes(this.state.filterDesc)
+      );
+    } else {
+      return positions;
+    }
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="budget__history">
+        <h1>History</h1>
+        <div className="budget__history--filter">
+          <span>Filter: </span>
+          <input
+            type="text"
+            name="filter_description"
+            placeholder="description..."
+            onChange={this.filterDesc}
+          />
         </div>
-        {props.positions.map(position => {
-          return (
-            <div
-              key={position.key}
-              className={filteredPositionStyle(position.value)}
-            >
-              <div className="cell budget__position__key">{position.key}.</div>
-              <div className="cell budget__position__desc">
-                {position.description}
-              </div>
-              <div className="cell budget__position__cost">
-                {position.value}
-              </div>
-              <div className="cell budget__position__buttons">
-                {/* EDIT BUTTON */}
-                <button
-                  className="button button__edit"
-                  onClick={() => handleDelete(position.key)}
-                >
-                  <img src={edit_icon} alt="edit-icon" />
-                </button>
+        <div className="budget__table">
+          <div className="budget__header">
+            <div className="budget__header__key">ID</div>
+            <div className="budget__header__desc">Description</div>
 
-                {/* DELETE BUTTON */}
-                <button
-                  className="button button__delete"
-                  onClick={() => handleDelete(position.key)}
-                >
-                  <img src={trash_icon} alt="trash-icon" />
-                </button>
+            <div className="budget__header__cost">Cost</div>
+
+            <div className="budget__header__buttons">Action</div>
+          </div>
+          {this.filterPositions().map(position => {
+            return (
+              <div
+                key={position.key}
+                className={this.filteredPositionStyle(position.value)}
+              >
+                <div className="cell budget__position__key">
+                  {position.key}.
+                </div>
+                <div className="cell budget__position__desc">
+                  {position.description}
+                </div>
+                <div className="cell budget__position__cost">
+                  {position.value}
+                </div>
+                <div className="cell budget__position__buttons">
+                  {/* EDIT BUTTON */}
+                  <button className="button button__edit">
+                    <img src={edit_icon} alt="edit-icon" />
+                  </button>
+
+                  {/* DELETE BUTTON */}
+                  <button
+                    className="button button__delete"
+                    onClick={() => this.handleDelete(position.key)}
+                  >
+                    <img src={trash_icon} alt="trash-icon" />
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default budgetPositions;
+export default BudgetPositions;
